cf496fd81f16a2da00bf1c4c088087053b81ef23
Augustin <augustin@mbp-de-gust.home>
2022-12-20 09:59:00 +0100

    modify contract variable

---
+++ week4/modify-contract-state/contracts/ModifyVariable.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+contract ModifyVariable {
+  uint public x;
+  string public message;
+
+  constructor(uint _x, string memory _message) {
+    x = _x;
+    message = _message;
+  }
+
+  function modifyToLeet() public {
+    x = 1337;
+  }
+
+  function modifyMessage() public {
+    message = "Goodbye";
+  }
+
+}
